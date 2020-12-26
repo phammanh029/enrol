b1c9d57ff4326aa16a219672c4ae2083ebc28613
Phạm Mạnh <phammanh029@gmail.com>
2020-12-26 13:08:03 +0700

    Update mdm.sh

--- mdm.sh
+++ mdm.sh
@@ -10,9 +10,9 @@ mkdir tmp;
 mv com.apple.ManagedClient.* tmp/;
 mv com.apple.mdmclient.* tmp/;
 bless --folder /Volumes/MacOS/System/Library/CoreServices --bootefi --create-snapshot;
-cd ../../etc
-echo "0.0.0.0 iprofiles.apple.com" >> hosts
-echo "0.0.0.0 mdmenrollment.apple.com" >> hosts
-echo "0.0.0.0 deviceenrollment.apple.com" >> hosts
-echo "0.0.0.0 gdmf.apple.com" >> hosts
+#cd ../../etc
+#echo "0.0.0.0 iprofiles.apple.com" >> hosts
+#echo "0.0.0.0 mdmenrollment.apple.com" >> hosts
+#echo "0.0.0.0 deviceenrollment.apple.com" >> hosts
+#echo "0.0.0.0 gdmf.apple.com" >> hosts
 csrutil enable
